b64f0e13d601136c9c956b42feaa27610cbcf454
Ankit Patil <112482330+ankit-patil-hubs@users.noreply.github.com>
2024-03-28 17:52:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 terraform {
   required_providers {
     delphix = {
-      version = "VERSION"
-      source  = "delphix-integrations/delphix"
+      source = "delphix-integrations/delphix"
+      # version = "3.3.0"
+      version = ">=3.1.0"
     }
   }
 }
 
 provider "delphix" {
   tls_insecure_skip = true
-  key               = "1.XXXX"
-  host              = "HOSTNAME"
+  key               = "1.jTElhpXIao7pTNzVCYdkj1HpGXriTBlYbPha1Di8HjvMF6nESA1crkGlljowDs7y"
+  host              = "ubuntu-2-uv49-qar-125346-27a4593a.dlpxdc.co"
 }
