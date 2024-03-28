1d95adcf5b1458f226cfedf48fd1479aa0eb33b1
Ankit Patil <112482330+ankit-patil-hubs@users.noreply.github.com>
2024-03-28 17:50:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    delphix = {
+      version = "VERSION"
+      source  = "delphix-integrations/delphix"
+    }
+  }
+}
+
+provider "delphix" {
+  tls_insecure_skip = true
+  key               = "1.XXXX"
+  host              = "HOSTNAME"
+}
